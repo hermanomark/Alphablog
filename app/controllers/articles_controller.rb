f481fe66c2e81a7e0215c971476df598560c730d
Mark Hermano <hermano.mark.mh@gmail.com>
2018-09-18 12:49:32 +0800

    extract redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   # it's up to you to name this but it should be plural like @articles
   def index
     @articles = Article.all
@@ -10,7 +11,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def create
@@ -29,11 +30,10 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def update
-    @article = Article.find(params[:id])
     # very similar for create function we have to pass the whitelist article_params
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
@@ -44,7 +44,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
@@ -52,6 +51,9 @@ class ArticlesController < ApplicationController
 
   # whitelist params
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
     def article_params
       params.require(:article).permit(:title, :description)
