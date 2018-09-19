c645853bbcd204714adad2731555be3408b89b54
Mark Hermano <hermano.mark.mh@gmail.com>
2018-09-19 12:32:46 +0800

    style errors and messages partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
     #do something
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       # redirect to show path
       redirect_to article_path(@article)
     else
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
   def update
     # very similar for create function we have to pass the whitelist article_params
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -46,7 +46,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
