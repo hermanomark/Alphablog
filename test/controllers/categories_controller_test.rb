e6bf8987c614bda95afa5b7a027583f276e11b25
Mark Hermano <hermano.mark.mh@gmail.com>
2018-09-23 15:27:47 +0800

    added admin requirement to create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,6 +6,8 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     # in show we need to pass the id
     # in rails 5 we'll pass the @category object itself (not just id) since we're using IntegrationTest
     @category = Category.create(name: "sports")
+    # create an admin for the test
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
   
   test "should get categories index" do
@@ -15,6 +17,8 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    # we need to simulate the admin john to login
+    sign_in_as(@user, "password")
     get new_category_path
     assert_response :success
   end
@@ -22,5 +26,13 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get category_path(@category)
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    # aseert no differenct in category.count when this action happens, post to the create action the category with name sports
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category:{name: "sports"}}
+    end
+    assert_redirected_to categories_path 
   end
 end
